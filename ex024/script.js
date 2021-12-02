a142331fd1b454d97267e634a0ce534b0218be5d
ccga <cesarchabesalb@gmail.com>
2021-12-02 15:08:21 -0300

    atualizado

---
+++ ex024/script.js
@@ -0,0 +1,9 @@
+var NaoFazSentidoPorTrasDasCameras = "https://www.amazon.com.br/N%C3%A3o-Faz-Sentido-Felipe-Neto/dp/8577343936"
+var ATDUDMY = "https://www.amazon.com.br/Felipe-Trajet%C3%B3ria-Maiores-Youtubers-Brasil/dp/8555460867"
+var AVPTDC = "https://www.amazon.com.br/Felipe-Neto-Vida-tr%C3%A1s-c%C3%A2meras/dp/8555461014"
+var OMSFN = "https://www.amazon.com.br/Mundo-Segundo-Felipe-Neto-Verdades/dp/8555462096"
+var ASP = "https://www.amazon.com.br/Felipe-Neto-Acredite-Se-Puder/dp/8500500565"
+
+function url(varURL){
+    window.open(varURL, "_blank")
+}
